8d2218caaebdf70ab9e57f2cbc097795c7e82658
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-09-21 12:08:13 -0400

    finished

--- Lab_2-8.py
+++ Lab_2-8.py
@@ -1,7 +1,22 @@
-"""
+""""
 Create a Python file named lab_2-8.py
 A team needs to score 15 points to win a gold medal, between 12-14 points to win a silver medal, 
 and between 9-11 point to win a bronze medal. A team does not earn a medal if they earn 8 or fewer points.
 Write a program using nested if else statements where a user can input the number of points a team scored and the medal that they earned is displayed.
+"""""
+x = 15
 
-"""
+if x >= 15:
+    print ("gold medal.")
+if x >12:
+    print ("you are in silver.")
+elif x > 14:
+    print ("you are out of silver.")
+if x > 9:
+    print ("you are in bronze.")
+elif x < 9:
+    print ("you are out of bronze")
+if x < 8: 
+    print ( "you failed.")
+
+
